ddd5414ae79fe0ec493b22cfce5e2c5c951f221b
Mathieu Ibersien <mathieu.ibersien@etu.univ-lehavre.fr>
2021-04-12 14:01:30 +0200

    fin du parallaxScrolling

---
+++ scrollMagic_Paralax/app.js
@@ -0,0 +1,41 @@
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box1',
+    reverse: false
+})
+.setClassToggle('.box1', 'fade-in')
+.addIndicators({
+    name: 'INDICATIONS',
+    indent: 200,
+    colorStart: '#fff'
+})
+.addTo(controller)
+
+var controller2 = new ScrollMagic.Controller();
+
+var scene2 = new ScrollMagic.Scene({
+    triggerElement: '.box2',
+    reverse: false
+})
+.setClassToggle('.box2', 'fade-in')
+.addIndicators({
+    name: 'INDICATIONS',
+    indent: 200,
+    colorStart: '#fff'
+})
+.addTo(controller2)
+
+var controller3 = new ScrollMagic.Controller();
+
+var scene3 = new ScrollMagic.Scene({
+    triggerElement: '.box3',
+    reverse: false
+})
+.setClassToggle('.box3', 'fade-in')
+.addIndicators({
+    name: 'INDICATIONS',
+    indent: 200,
+    colorStart: '#fff'
+})
+.addTo(controller3)
