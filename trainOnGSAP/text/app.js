33dfce6a893befd81f7f25424337349df5b6f46e
Mathieu Ibersien <mat98-76@live.fr>
2021-05-10 21:24:40 +0200

    developer presentation gsap text

--- trainOnGSAP/text/app.js
+++ trainOnGSAP/text/app.js
@@ -1,33 +1,21 @@
-const timeline = gsap.timeline();
+const words = [ "Mathieu", "a french developer" ]
 
-timeline.to("#text", {
-    delay: 1,
-    text: {
-        speed: 1,
-        value: "Vue.js",
-        delimiter: "",
-        newClass: "vue",
-        oldClass: "normal"
-    },
-    ease: "none"
-}).to('#text', {
-    delay: 1,
-    duration: 1,
-    text: {
-        value: "Angular",
-        delimiter: "",
-        newClass: "angular",
-        oldClass: "normal"
-    },
-    ease: "power2"
-}).to('#text', {
-    delay: 1,
-    text: {
-        speed: 1,
-        padSpace: true,
-        value: "React",
-        delimiter: "",
-        newClass: "react",
-    },
-    ease: "none"
-});
+gsap.to('.cursor', { duration: 0.7, opacity: 0, repeat: -1 })
+
+let masterTL = gsap.timeline({ repeat: -1 })
+
+masterTL
+.from(".box", { duration: 1, x: "-50rem", ease: "power3.out"})
+/* .from('.hi', { duration: 1, y: "50rem", ease: "power1.out"})
+.to('.box', { height: "7rem" })
+
+words.forEach(word => {
+    let tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1});
+    tl.to(".text", {
+        duration: 1,
+        text: {
+            value: word,
+        }
+    })
+    masterTL.add(tl);
+}) */
