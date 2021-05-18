927c2606e7105cde21a763ad4669c7c8a430ca6c
Mathieu Ibersien <mat98-76@live.fr>
2021-05-18 22:06:10 +0200

    FIX Shaka ponk

--- trainOnGSAP/shaka_ponk/app.js
+++ trainOnGSAP/shaka_ponk/app.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const title = document.querySelectorAll('.title h1 span')
-
+console.log(title)
 const span1 = title[0]
 const span2 = title[2]
 
@@ -15,11 +15,13 @@ charming(span2, {
 const letters1 = Array.from(span1.childNodes)
 const letters2 = Array.from(span2.childNodes)
 
+console.log(letters1)
+
 const timeline = gsap.timeline({
   scrollTrigger: {
     trigger: ".background",
     start: 'top top',
-    scrub: 1,
+    scrub: true,
     /*   markers: {
     startColor: "green",
     endColor: "red",
@@ -31,7 +33,7 @@ const timeline = gsap.timeline({
 })
 
 timeline.to('.social-network', { y: -400 })
-  .to('.blur', { y: -550 }, "<")
+  .to('.blur', { y: -250 }, "<")
 
 for (let i = 0; i < letters1.length; i++) {
   timeline.to(`.shaka${i + 1}`, {
