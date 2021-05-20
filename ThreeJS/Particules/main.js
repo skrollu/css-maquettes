dc323aa53295414475e37f585a8af451b6ab5c60
Mathieu Ibersien <mat98-76@live.fr>
2021-05-20 16:40:20 +0200

    Création de projet avec Vite js et fin de l'intro avec le cube

--- ThreeJS/Particules/main.js
+++ ThreeJS/Particules/main.js
@@ -1,28 +1,28 @@
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+import { Scene, PerspectiveCamera, AxesHelper, WebGLRenderer, Mesh, BoxBufferGeometry, MeshNormalMaterial} from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new Scene();
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-scene.add(new THREE.AxesHelper(1));
+scene.add(new AxesHelper(1));
 scene.add(camera);
 camera.position.z = 2;
 camera.position.y = 0.5;
 camera.position.x = 0.5;
 
-const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1,1,1), new THREE.MeshNormalMaterial()) //obj composé d'une géométrie quiest un ens de pts pour créer des formes et un Material qui permet d'y poser des textures (simplifié)
+const cube = new Mesh(new BoxBufferGeometry(1,1,1), new MeshNormalMaterial()) //obj composé d'une géométrie quiest un ens de pts pour créer des formes et un Material qui permet d'y poser des textures (simplifié)
 scene.add(cube)
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer({ antialias: true }); //antialias enlève l'alisaing sur les bords du cube
 renderer.setSize( window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); //Evite de trop détailler le rendu ce qui augmente la perte de performance si l'ecran a une résolution forte
 document.body.appendChild( renderer.domElement ); //créer un element canvas dans le dom
 
-const controls = new OrbitControls(camera, renderer)
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function tick() {
     renderer.render(scene, camera)
-/*     camera.position.x += 0.01;
-    camera.lookAt(0,0,0) */
+    controls.update()
     requestAnimationFrame(tick)
 }
 
