46fcad899830e4562cbb23e2943e505592032b70
Mathieu Ibersien <mat98-76@live.fr>
2021-05-30 18:21:58 +0200

    Alpha map three js project started but photoshop is needed to pursive...

---
+++ ThreeJS/alphaMap/main.js
@@ -0,0 +1,107 @@
+import './style.css'
+import * as THREE from 'three'
+//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+//Texture loader
+const loader = new THREE.TextureLoader()
+const height = loader.load('./img/height.png')
+const alpha = loader.load('./img/alpha.png')
+const texture = loader.load('./img/texture.jpg')
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.PlaneBufferGeometry(2,2,64,64)
+const material = new THREE.MeshStandardMaterial({
+  color: "gray",
+  map: texture
+})
+const plane = new THREE.Mesh(geometry, material)
+scene.add(plane);
+plane.rotation.x = 181;
+
+gui.add(plane.rotation, 'x',0,600)
+
+// Lights
+const pointLight = new THREE.PointLight(0xffffff, 1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+gui.add(pointLight.position, 'x', 0 , 10)
+gui.add(pointLight.position, 'y', 0 , 10)
+gui.add(pointLight.position, 'z', 0 , 10)
+
+const color = { color: '#00ff00' }
+gui.addColor(color, 'color').onChange(() => {
+  pointLight.color.set(color.color)
+})
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    // Update objects
+
+    // Render
+    renderer.render(scene, camera)
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
