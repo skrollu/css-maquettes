45d582b90be479798a4405bfe002cb7f8ac6527f
Mathieu Ibersien <mat98-76@live.fr>
2021-11-11 17:19:32 +0100

    erase swup legacy files

--- app.js
+++
@@ -1,9 +0,0 @@
-const options = {
-    linkSelector:
-        'a[href^="' +
-        window.location.origin +
-        '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
-    plugins: [new SwupDebugPlugin()]
-};
-
-const swup = new Swup(options);
